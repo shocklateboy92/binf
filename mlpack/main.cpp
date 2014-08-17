48d0e75d75b73fc61c2ef4794abb3f21c64aa15a
Lasath Fernando <kde@lasath.org>
2014-08-18 09:03:04 +1000

    Make mlpack use the newly created format

--- mlpack/main.cpp
+++ mlpack/main.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
+#include <fstream>
 #include <mlpack/core.hpp>
 #include <mlpack/methods/hmm/hmm.hpp>
 #include <mlpack/core/dists/discrete_distribution.hpp>
 #include <armadillo>
 #include <armadillo_bits/arma_cmath.hpp>
 
-using namespace std;
+//using namespace std;
 using namespace mlpack;
+using namespace arma;
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::size_t num_states = 2;
-    arma::vec initial = {0.5, 0.5};
-    arma::mat transitions = {
-        0.1d, 0.9d,
-        0.4d, 0.6d
-    };
-    transitions.reshape(num_states, num_states);
-    std::vector<distribution::DiscreteDistribution> emissions = {
-        distribution::DiscreteDistribution(num_states),
-        distribution::DiscreteDistribution(num_states)
-    };
+    if (argc < 2) {
+        return -1;
+    }
+    std::ifstream is(argv[1]);
 
-    hmm::HMM<distribution::DiscreteDistribution> model(transitions, emissions);
-    arma::mat observations = {1, 0};
+    std::size_t num_states = 2;
+    std::size_t num_symbols = 2;
+    std::size_t num_observations = 2;
+    is >> num_states;
+    is >> num_symbols;
+    is >> num_observations;
+
+    arma::mat *transitions = new arma::mat(num_states, num_states);
+    std::vector<distribution::DiscreteDistribution> emissions;
+    arma::rowvec observations(num_observations);
+
+    for (std::size_t row = 0; row < num_states; row++) {
+        for (std::size_t col = 0; col < num_states; col++) {
+            assert(is.good());
+            is >> (*transitions)(row, col);
+        }
+    }
+
+    for (std::size_t row = 0; row < num_states; row++) {
+        assert(is.good());
+        arma::vec probs(num_symbols);
+        for (std::size_t col = 0; col < num_symbols; col++) {
+            is >> probs(col);
+        }
+        emissions.push_back(distribution::DiscreteDistribution(probs));
+    }
+
+    for (std::size_t col = 0; col < num_observations; col++) {
+        assert(is.good());
+        is >> observations(col);
+    }
+
+    hmm::HMM<distribution::DiscreteDistribution> model(*transitions, emissions);
     arma::Col<std::size_t> results;
     double score = model.Predict(observations, results);
     std::cout << results;
