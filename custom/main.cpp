df4480a44648ce2985a8800f9681c9eacd396e99
Lasath Fernando <kde@lasath.org>
2014-08-19 13:28:01 +1000

    Implement hacky sparse hmm viterbi
    I should fix this up and make it evaluate the result.

--- custom/main.cpp
+++ custom/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <unordered_map>
 #include<limits>
 
 #include <boost/multi_array.hpp>
@@ -10,6 +11,20 @@
 
 typedef std::vector<double> vec;
 typedef boost::multi_array<double, 2> mat;
+
+struct state {
+    std::size_t id;
+    std::unordered_map<std::size_t, double> transitions;
+//    std::vector<std::pair<std::size_t, double>> transitions;
+};
+
+struct partV {
+    double prob;
+    state *s;
+
+    partV() : prob(0), s(nullptr) {}
+    partV(double _prob, state *_s) : prob(_prob), s(_s) {}
+};
 
 int main(int argc, char *argv[])
 {
@@ -28,11 +43,18 @@ int main(int argc, char *argv[])
     mat transitions(boost::extents[num_states][num_states]);
     mat emissions(boost::extents[num_states][num_states]);
     vec observations(num_states);
+    std::vector<state> states(num_states);
 
     for (std::size_t row = 0; row < num_states; row++) {
         for (std::size_t col = 0; col < num_states; col++) {
             assert(is.good());
-            is >> transitions[row][col];
+//            is >> transitions[row][col];
+            double val;
+            is >> val;
+            if (val) {
+                states[row].transitions[col] = val;
+            }
+            states[row].id = row;
         }
     }
 
@@ -48,28 +70,42 @@ int main(int argc, char *argv[])
         is >> observations[row];
     }
 
-    mat V(boost::extents[num_observations][num_states]);
+//    mat V(boost::extents[num_observations][num_states]);
+    std::vector<std::unordered_map<std::size_t, partV>> V(num_observations);
 
-    for (std::size_t s = 0; s < num_states; s++) {
-        V[0][s] = transitions[0][s] * emissions[s][observations[0]];
+    for (state &s : states) {
+        partV v = { 1.0d / (double) num_states, &s};
+        V[0][s.id] = v;
+//        std::cout << v.prob << " ";
+//        for (std::size_t i = 0; i < num_states; i++) {
+//            V[0][i].prob = 1.0d / (double) num_states;
+//        }
     }
 
     for (std::size_t t = 1; t < num_observations; t++) {
-        for (std::size_t s1 = 1; s1 < num_states; s1++) {
-            double max = std::numeric_limits<double>::min();
-            std::size_t state_max = -1;
-            for (std::size_t s2 = 0; s2 < num_states; s2++) {
-                double prob = V[t-1][s2] *
-                        transitions[s2][s1] *
-                        emissions[s1][observations[t]];
-                if (prob > max) {
-                    max = prob;
-                    state_max = s2;
+        for (auto v : V[t-1]) {
+//            double max = std::numeric_limits<double>::min();
+            for (std::pair<std::size_t, double> transition : states[v.first].transitions) {
+                std::size_t sid = transition.first;
+                double p2 = v.second.prob *
+                        transition.second *
+                        emissions[sid][observations[t]];
+                if (V[t][transition.first].prob < p2) {
+                    V[t][transition.first].prob = p2;
+                    V[t][transition.first].s = &states[transition.first];
                 }
             }
-            V[t][s1] = max;
         }
     }
+
+//    for (auto v : V) {
+//        for (auto p : v) {
+////            std::cout << p.first << ":" << p.second.prob << " ";
+////            std::cout << p.count();
+//        }
+//        std::cout << v.size();
+//        std::endl(std::cout);
+//    }
 
     return 0;
 }
