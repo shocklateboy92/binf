be718a9457d2d0a8ebb99627c2f41336e7254ed6
Lasath Fernando <kde@lasath.org>
2014-08-20 12:49:35 +1000

    Fix up sparse hmm viterbi implementation
    It still uses linear scaling - should probably change that to log.

--- custom/main.cpp
+++ custom/main.cpp
@@ -11,20 +11,10 @@
 
 typedef std::vector<double> vec;
 typedef boost::multi_array<double, 2> mat;
+typedef std::vector<std::unordered_map<std::size_t, double>> s_mat;
+typedef std::pair<std::size_t, double> s_val;
 
-struct state {
-    std::size_t id;
-    std::unordered_map<std::size_t, double> transitions;
-//    std::vector<std::pair<std::size_t, double>> transitions;
-};
-
-struct partV {
-    double prob;
-    state *s;
-
-    partV() : prob(0), s(nullptr) {}
-    partV(double _prob, state *_s) : prob(_prob), s(_s) {}
-};
+static const double SCALE = 4.0d;
 
 int main(int argc, char *argv[])
 {
@@ -36,28 +26,28 @@ int main(int argc, char *argv[])
     std::ifstream is(argv[1]);
     assert(is.is_open());
 
+    // read model info
     is >> num_states;
     is >> num_symbols;
     is >> num_observations;
 
-    mat transitions(boost::extents[num_states][num_states]);
+    s_mat transitions(num_states);
     mat emissions(boost::extents[num_states][num_states]);
     vec observations(num_states);
-    std::vector<state> states(num_states);
 
+    // read transition probabilities between states
     for (std::size_t row = 0; row < num_states; row++) {
         for (std::size_t col = 0; col < num_states; col++) {
             assert(is.good());
-//            is >> transitions[row][col];
             double val;
             is >> val;
             if (val) {
-                states[row].transitions[col] = val;
+                transitions[row][col] = val;
             }
-            states[row].id = row;
         }
     }
 
+    // read emission probabilities of each state
     for (std::size_t row = 0; row < num_states; row++) {
         for (std::size_t col = 0; col < num_symbols; col++) {
             assert(is.good());
@@ -65,47 +55,40 @@ int main(int argc, char *argv[])
         }
     }
 
+    // read the observations
     for (std::size_t row = 0; row < num_observations; row++) {
         assert(is.good());
         is >> observations[row];
     }
 
-//    mat V(boost::extents[num_observations][num_states]);
-    std::vector<std::unordered_map<std::size_t, partV>> V(num_observations);
+    s_mat V(num_observations);
 
-    for (state &s : states) {
-        partV v = { 1.0d / (double) num_states, &s};
-        V[0][s.id] = v;
-//        std::cout << v.prob << " ";
-//        for (std::size_t i = 0; i < num_states; i++) {
-//            V[0][i].prob = 1.0d / (double) num_states;
-//        }
-    }
+    // set state 0 as the initial state
+    V[0][0] = 1.0d;
 
+    // do viterbi iterative viterbi algorithm
     for (std::size_t t = 1; t < num_observations; t++) {
-        for (auto v : V[t-1]) {
-//            double max = std::numeric_limits<double>::min();
-            for (std::pair<std::size_t, double> transition : states[v.first].transitions) {
-                std::size_t sid = transition.first;
-                double p2 = v.second.prob *
+        for (s_val v : V[t-1]) {
+            for (s_val transition : transitions[v.first]) {
+                double v_next = v.second *
                         transition.second *
-                        emissions[sid][observations[t]];
-                if (V[t][transition.first].prob < p2) {
-                    V[t][transition.first].prob = p2;
-                    V[t][transition.first].s = &states[transition.first];
+                        SCALE *
+                        emissions[transition.first][observations[t-1]];
+                if (V[t][transition.first] <= v_next) {
+                    V[t][transition.first] = v_next;
                 }
             }
         }
     }
 
-//    for (auto v : V) {
-//        for (auto p : v) {
-////            std::cout << p.first << ":" << p.second.prob << " ";
-////            std::cout << p.count();
-//        }
-//        std::cout << v.size();
-//        std::endl(std::cout);
-//    }
+    // print the most likely path
+    for (std::unordered_map<std::size_t, double> v : V) {
+        auto max = *std::max_element(v.begin(), v.end(),
+                                    [](s_val a, s_val b){
+                                        return a.second < b.second;
+                                    });
+        std::cout << max.first << ":" << max.second << std::endl;
+    }
 
     return 0;
 }
