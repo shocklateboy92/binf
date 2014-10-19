c7f1ec79123563c05aad1718210e4da3da1b4bcf
Lasath Fernando <kde@lasath.org>
2014-10-19 18:10:51 +1100

    Create python script to parse output of 'time'

---
+++ reportA/time-bench/format.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os, sys
+import fileinput
+import re
+
+for f in sys.argv[1:]:
+    fnum = int(re.match('results(\d)\.txt', f).group(1))
+    vals = []
+    run = 0
+    for line in open(f):
+        res = re.search('real\D+(\d+)m([\d.]+)s', line)
+        if res:
+            time = int(res.group(1)) * 60 + float(res.group(2))
+            vals.append("({}, {})".format(run, time))
+            run += 1
+
+    print(r"\addplot coordinates {{{}}};".format(" ".join(vals)))
+    print(r"\addlegendentry{{n = {}}}".format(fnum))
+
+
