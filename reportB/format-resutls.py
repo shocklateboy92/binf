18d503b6d95cd4b79b64130750bd1db9a59f46ef
Lasath Fernando <kde@lasath.org>
2015-06-01 20:42:23 +1000

    Write script to format data into tex

---
+++ reportB/format-resutls.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os, sys
+import fileinput
+import re
+import statistics
+
+vals = []
+
+for line in fileinput.input():
+    res = re.search('real\D+(\d+)m([\d.]+)s', line)
+    if res:
+        time = int(res.group(1)) * 60 + float(res.group(2))
+        vals.append(time)
+
+mid = round(len(vals) / 2)
+begin = vals[0:mid]
+end = vals[mid:len(vals)]
+
+output = r"""
+    \addplot+[
+        boxplot prepared={{
+            lower whisker={},
+            lower quartile={},
+            median={},
+            upper quartile={},
+            upper whisker={},
+        }},
+    ]
+""".format(
+        min(vals),
+        statistics.median(vals),
+        statistics.median(begin),
+        statistics.median(end),
+        max(vals)
+    )
+
+print (output)
