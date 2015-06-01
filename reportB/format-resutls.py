fc1949b2cc91f332e1f2564d47c54e422c492cbe
Lasath Fernando <kde@lasath.org>
2015-06-02 02:15:03 +1000

    Add graph as squashed box-plot
    The way the wrapfigure has its height set is super sketchy! Fix
    plx.

--- reportB/format-resutls.py
+++ reportB/format-resutls.py
@@ -5,35 +5,38 @@ import fileinput
 import re
 import statistics
 
-vals = []
+for f in ["i1-results.txt", "i2-results.txt"]:
+    vals = []
 
-for line in fileinput.input():
-    res = re.search('real\D+(\d+)m([\d.]+)s', line)
-    if res:
-        time = int(res.group(1)) * 60 + float(res.group(2))
-        vals.append(time)
+    for line in open(f):
+        res = re.search('real\D+(\d+)m([\d.]+)s', line)
+        if res:
+            time = int(res.group(1)) * 60 + float(res.group(2))
+            vals.append(time)
 
-mid = round(len(vals) / 2)
-begin = vals[0:mid]
-end = vals[mid:len(vals)]
+    vals.sort()
 
-output = r"""
-    \addplot+[
-        boxplot prepared={{
-            lower whisker={},
-            lower quartile={},
-            median={},
-            upper quartile={},
-            upper whisker={},
-        }},
-    ]
-    coordinates{{}};
-""".format(
-        min(vals),
-        statistics.median(vals),
-        statistics.median(begin),
-        statistics.median(end),
-        max(vals)
-    )
+    mid = round(len(vals) / 2)
+    begin = vals[0:mid]
+    end = vals[mid:len(vals)]
 
-print (output)
+    output = r"""
+        \addplot+[
+            boxplot prepared={{
+                lower whisker={},
+                lower quartile={},
+                median={},
+                upper quartile={},
+                upper whisker={},
+            }},
+        ]
+        coordinates{{}};
+    """.format(
+            min(vals),
+            statistics.median(vals),
+            statistics.median(begin),
+            statistics.median(end),
+            max(vals)
+        )
+
+    print (output)
