12ada42cf459f7368342581450ac08da2907c2e1
Lasath Fernando <kde@lasath.org>
2015-06-02 11:16:26 +1000

    Increase sample size of benchmark scripts
    For overnight run.

--- bench-scripts/i2-thread-runs.sh
+++ bench-scripts/i2-thread-runs.sh
@@ -3,7 +3,7 @@
 CMD="/home/fernie/build/CMakeLists.txt-Desktop_GCC-Default/ih-generate-models blastOutput2.xml example2.fa"
 
 for t in {1..12}; do
-    for j in {1..1}; do
+    for j in {1..20}; do
         { time $CMD $t; } 2>> i2-results${t}.txt;
     done;
 done;
